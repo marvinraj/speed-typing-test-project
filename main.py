8ad1f09ced8e18dc4c29a5eece5bc24a0b696e00
Marvin <marvinraj77@gmail.com>
2024-02-01 19:51:02 +0800

    minor changes

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def main():
 
     # calculate time taken to type out the sentence
     time_taken = end - start    
-    print("time taken : " + str(time_taken))
+    print("time taken : " + str(round(time_taken, 2)))
 
     # calculate the wpm/speed 
     wpm = len(user_input)*60/(5*time_taken)
-    print("wpm : " + str(wpm))
+    print("wpm : " + str(round(wpm, 2)))
 
 if __name__ == "__main__":
     main()
