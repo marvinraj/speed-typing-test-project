2acd87d16339a6fcd67322d055da538c94a32a45
Marvin <marvinraj77@gmail.com>
2024-02-01 19:47:22 +0800

    calculate time and wpm (basic)

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# coming soooooonnnn
+import time
+
+
+def main():
+
+    to_start = input("press enter to start!")
+    
+    # start timer
+    start = time.time()
+    user_input = input("enter your name now: ")
+    end = time.time()
+    # end timer
+
+    # calculate time taken to type out the sentence
+    time_taken = end - start    
+    print("time taken : " + str(time_taken))
+
+    # calculate the wpm/speed 
+    wpm = len(user_input)*60/(5*time_taken)
+    print("wpm : " + str(wpm))
+
+if __name__ == "__main__":
+    main()
