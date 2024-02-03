806f7ed72cf9064762e1c4bc360179ef7fb62787
Marvin <marvinraj77@gmail.com>
2024-02-03 10:37:34 +0800

    integrated api ninja to get random qoutes

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import time
+import requests
 
 
 def main():
+
+    category = "happiness"
+    api_url = "https://api.api-ninjas.com/v1/quotes?category={}".format(category)
+    response = requests.get(api_url, headers={"X-Api-Key": "n4FilKP0NZDqYZguKKP1+Q==TePugoKqGLVK4rQJ"})
+
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print("Error:", response.status_code, response.text)
 
     to_start = input("press enter to start!")
     
